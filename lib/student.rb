6b475757327582c3633debc48f76c3fd1564ca79
Rima Jean <rima.jean@gmail.com>
2018-11-08 15:21:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,14 +67,14 @@ end
   end
 end
 
-  def self.first_x_students_in_grade_10(x)
+  def self.first_x_students_in_grade_10(num)
     sql = <<-SQL
     SELECT *
     FROM students
     WHERE grade = 10
     LIMIT ?
     SQL
-    DB[:conn].execute(sql, x).collect do |row|
+    DB[:conn].execute(sql, num).collect do |row|
       self.new_from_db(row)
   end
 end
