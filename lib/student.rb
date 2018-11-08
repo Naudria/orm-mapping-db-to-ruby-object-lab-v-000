8d24d24e403681312db2dc5214110ad0a9cfe256
Rima Jean <rima.jean@gmail.com>
2018-11-08 14:37:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,16 +7,16 @@ class Student
     student.id = row[0]
     student.name = row[1]
     student.grade = row[2]
-    student 
+    student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
-    sql = <<-SQL 
+    sql = <<-SQL
     SELECT *
     FROM students
-    SQL 
+    SQL
     DB[:conn].execute(sql).map do |row|
     self.new_from_db(row)
   end
@@ -24,17 +24,26 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE name = ?
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first 
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
